9bfd074ef9cc22809a9fc79f801d2d3985e9bc02
imanankil <155680805+imanankil@users.noreply.github.com>
2024-01-23 14:55:17 -0600

    20.Passing data to components props Update Post.jsx Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Post from './components/Post';
-
+  
 
 function App() {
   return (
    <main>
-   <Post/>
-   <Post/>
+   <Post author="Maximilian" body="React.js is awesome"/>
+   <Post author="Manuel" body="Check out the full course!"/>
+
    <Post/>
 
 
