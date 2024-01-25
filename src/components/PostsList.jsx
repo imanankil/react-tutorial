3ddd4d6043cda4421f57b2ce8e18a6f3e5b110d7
imanankil <155680805+imanankil@users.noreply.github.com>
2024-01-25 00:50:17 -0600

    Outputting List Data Update PostsList.jsx
    Modole 33

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,9 +4,9 @@ import NewPost from './NewPost';
 import Modal from './Modal'
 import classes from './PostsList.module.css';
     function PostsList ({isPosting, onStopPosting}) {
-    const [post, setPost] = useState([]);
+    const [posts, setPosts] = useState([]);
 
-    function addPostHandler(postDate) {
+    function addPostHandler(postData) {
         setPosts((existingPosts) => [postData, ...existingPosts]);
     }
         return (
@@ -16,10 +16,18 @@ import classes from './PostsList.module.css';
             <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>
         </Modal>
             )}
-        
-            <ul className={classes.posts}>
-                <Post author="Manuel" body="Check out the full course!"/>
-            </ul>
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+                </ul>
+            )}
+            {posts.length ===0 && (
+            <div style={{textAlign: 'center', color:'white'}}>
+                <h2>There are no posts yet</h2>
+                <p>Start adding some!</p>
+            </div>
+
+            )}
         </>
     );
 }
